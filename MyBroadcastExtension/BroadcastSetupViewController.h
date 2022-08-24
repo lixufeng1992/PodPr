82e46f7c08ce056818ffd08f4d843ce1a9f05079
lixufeng.lxf <lixufeng.lxf@bytedance.com>
2022-08-24 14:56:04 +0800

    初始化

---
+++ MyBroadcastExtension/BroadcastSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  BroadcastSetupViewController.h
+//  MyBroadcastExtension
+//
+//  Created by lixufeng on 2022/8/24.
+//
+
+#import <UIKit/UIKit.h>
+#import <ReplayKit/ReplayKit.h>
+
+@interface BroadcastSetupViewController : UIViewController
+
+@end
